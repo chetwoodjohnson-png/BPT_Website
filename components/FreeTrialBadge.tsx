582900f5a4c86a792e18c5033f416ad92281a4f1
Chetwood Johnson <chetwood.johnson@gmail.com>
2026-06-27 09:04:33 -0400

    Move trial badge below nav

--- components/FreeTrialBadge.tsx
+++ components/FreeTrialBadge.tsx
@@ -2,7 +2,15 @@
 
 export default function FreeTrialBadge() {
   return (
-    <div style={{ position: "fixed", top: "140px", right: "20px", zIndex: 10, pointerEvents: "none" }}>
+    <div
+      style={{
+        position: "fixed",
+        top: "220px",
+        right: "20px",
+        zIndex: 10,
+        pointerEvents: "none",
+      }}
+    >
       <a 
         href="https://bpmscloud.com" 
         target="_blank" 
